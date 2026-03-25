90dd8e066571a82a14e70af2de7ddf5a5abf37c7
atul <atul@example.com>
2026-03-25 19:31:10 +0530

    handle error for name

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -561,9 +561,9 @@
                                             <div class="d-flex align-items-center">
                                                 <div
                                                     class="avatar avatar-sm bg-primary text-white rounded-circle me-2 d-flex align-items-center justify-content-center">
-                                                    <span>{{ substr($appointment->user->name, 0, 1) }}</span>
+                                                    <span>{{ substr($appointment?->user?->name, 0, 1) }}</span>
                                                 </div>
-                                                {{ $appointment->user->name ?? '-' }}
+                                                {{ $appointment?->user?->name ?? '-' }}
                                             </div>
                                         </td>
                                         <td>
